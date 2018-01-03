320480020a6c0042a454eceb06daefd3fbff5a82
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-03 09:05:07 +0100

    user login

---
+++ src/components/Admin/LogIn.vue
@@ -0,0 +1,76 @@
+<template>
+   <section class="section">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+
+        <h2 class="title has-text-centered">Login</h2>
+
+        <form method="POST" @submit.prevent="login">
+          <div class="field">
+            <label class="label">E-Mail Address</label>
+
+            <p class="control">
+              <input
+                type="email"
+                class="input"
+                v-model="email">
+            </p>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+
+            <p class="control">
+              <input
+                type="password"
+                class="input"
+                v-model="password">
+            </p>
+          </div>
+
+          <p class="control">
+            <button class="button is-primary is-fullwidth is-uppercase">Login</button>
+          </p>
+        </form>
+      </div>
+    </div>
+   </section>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'LogIn',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation($email: String!, $password: String!) {
+              login(email: $email, password: $password)
+            }
+          `,
+          // parameters
+          variables: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        .then(response => {
+          // save user token to localstorage
+          localStorage.setItem('blog-app-token', response.data.login)
+
+          // redirect to admin dashboard
+          this.$router.replace('/admin')
+        })
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import SignUp from '@/components/Admin/SignUp'
+import LogIn from '@/components/Admin/LogIn'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'SignUp',
       component: SignUp
+    },
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogIn
     }
   ]
 })
