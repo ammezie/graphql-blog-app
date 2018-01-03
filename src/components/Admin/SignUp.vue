46dd5328cc0127f9530505fc1f0d7760c4f928cb
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-03 09:04:31 +0100

    user signup

---
+++ src/components/Admin/SignUp.vue
@@ -0,0 +1,97 @@
+<template>
+   <section class="section">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+
+        <h2 class="title has-text-centered">Signup</h2>
+
+        <form method="POST" @submit.prevent="signup">
+          <div class="field">
+            <label class="label">Username</label>
+
+            <p class="control">
+              <input
+                type="text"
+                class="input"
+                v-model="username">
+            </p>
+          </div>
+
+          <div class="field">
+            <label class="label">E-Mail Address</label>
+
+            <p class="control">
+              <input
+                type="email"
+                class="input"
+                v-model="email">
+            </p>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+
+            <p class="control">
+              <input
+                type="password"
+                class="input"
+                v-model="password">
+            </p>
+          </div>
+
+          <p class="control">
+            <button class="button is-primary is-fullwidth is-uppercase">SignUp</button>
+          </p>
+        </form>
+      </div>
+    </div>
+   </section>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signup () {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation($username: String!, $email: String!, $password: String!) {
+              createUser(
+                username: $username
+                email: $email
+                password: $password
+              ) {
+                id
+                username
+                email
+              }
+            }
+          `,
+          // parameters
+          variables: {
+            username: this.username,
+            email: this.email,
+            password: this.password
+          }
+        })
+        .then(response => {
+          // clear inputs
+          this.username = this.email = this.password = ''
+
+          // redirect to login page
+          this.$router.replace('/login')
+        })
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import SignUp from '@/components/Admin/SignUp'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
