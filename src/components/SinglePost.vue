cad1c50e4a385fc70719669a78cac1b185e991f9
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-04 22:52:11 +0100

    show single post

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <router-link class="button is-link is-small" to="/">Back Home</router-link>
+
+        <h1 class="title">
+          {{ post.title }}
+        </h1>
+
+        <div class="content">
+          {{ post.content }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { POST_QUERY } from '@/graphql'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      post: '',
+      slug: this.$route.params.slug
+    }
+  },
+  apollo: {
+    // fetch post by slug
+    post: {
+      query: POST_QUERY,
+      variables () {
+        return {
+          slug: this.slug
+        }
+      }
+    }
+  }
+}
+</script>
