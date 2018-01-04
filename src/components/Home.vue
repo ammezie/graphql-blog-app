bf568c2c0900197dd3f29446f42f623d7e225160
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-04 22:51:53 +0100

    homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-6 is-offset-3">
+        <h1 class="title">Latest Posts</h1>
+
+        <h3
+          v-for="post in allPosts"
+          :key="post.id"
+          class="title is-5">
+          <router-link :to="post.slug">
+            {{ post.title }}
+          </router-link>
+        </h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ALL_POSTS_QUERY } from '@/graphql'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      allPosts: []
+    }
+  },
+  apollo: {
+    // fetch all posts
+    allPosts: {
+      query: ALL_POSTS_QUERY
+    }
+  }
+}
+</script>
